26b8e88c11f8e70e7c2143c308e1a0f98f690750
Manali Dighe <manalirahuldighe@gmail.com>
2026-05-26 21:12:07 +0530

    Add: single-number - submission-2

---
+++ Data Structures & Algorithms/single-number/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>m;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            m[nums[i]]++;
+        }
+        for(auto it:m)
+        {
+            if(it.second==1)
+            {
+                return it.first;
+            }
+        }
+        return -1;
+    }
+};
