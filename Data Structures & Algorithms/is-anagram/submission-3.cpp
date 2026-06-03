c15f47dcbb465c8b084f006825b274320fd6ba41
Manali Dighe <manalirahuldighe@gmail.com>
2026-06-03 11:53:07 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+       sort(s.begin(),s.end());
+       sort(t.begin(),t.end());
+       if(s.size()!=t.size())
+       {
+        return false;
+       } 
+       if(s.size()==t.size())
+       {
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==t[i])
+            {
+                continue;
+            }
+            else
+            {
+                return false;
+            }
+        }
+       } 
+       return true;
+    }
+};
