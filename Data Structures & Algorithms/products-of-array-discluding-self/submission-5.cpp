dc3e5727af3f0e0147aec63b2252f0c9d3832f09
Manali Dighe <manalirahuldighe@gmail.com>
2026-06-13 08:58:28 +0530

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int>v;
+        for(int i=0;i<nums.size();i++)
+        {
+            int mul=1;
+            for(int j=0;j<nums.size();j++)
+            {
+                if(i==j)
+                continue;
+                mul*=nums[j];
+            }
+            v.push_back(mul);
+        }
+        return v;
+    }
+};
